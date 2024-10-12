3afab4f63826b91f8caf67e3715f965de7ff023a
Dmitrii <dimterion59@gmail.com>
2024-10-12 12:41:39 +0200

    add header

---
+++ components/Header.jsx
@@ -0,0 +1,87 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/images/logo.png";
+import { FaUser, FaSignInAlt, FaSignOutAlt, FaCompass } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="bg-gray-100">
+      <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex items-center">
+            <Link href="/">
+              <Image
+                src={logo}
+                className="h-12 w-12"
+                alt="Booking Site logo"
+                priority={true}
+              />
+            </Link>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <Link
+                  href="/"
+                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
+                >
+                  Places
+                </Link>
+                <Link
+                  href="/"
+                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
+                >
+                  Bookings
+                </Link>
+                <Link
+                  href="/"
+                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
+                >
+                  Add Place
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="ml-auto">
+            <div className="ml-4 flex items-center md:ml-6">
+              <Link href="/" className="mr-3 text-gray-800 hover:text-gray-600">
+                <FaSignInAlt className="mr-1 inline" /> Login
+              </Link>
+              <Link href="/" className="mr-3 text-gray-800 hover:text-gray-600">
+                <FaUser className="mr-1 inline" /> Register
+              </Link>
+              <Link href="/">
+                <FaCompass className="mr-1 inline" /> My Places
+              </Link>
+              <Link href="/" className="mx-3 text-gray-800 hover:text-gray-600">
+                <FaSignOutAlt className="mr-1 inline" /> Sign Out
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="md:hidden">
+        <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+          <Link
+            href="/"
+            className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
+          >
+            Places
+          </Link>
+          <Link
+            href="/"
+            className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
+          >
+            Bookings
+          </Link>
+          <Link
+            href="/"
+            className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
+          >
+            Add Place
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
