05b7dec099380dc808086589e841161e3e14425f
Dmitrii <dimterion59@gmail.com>
2024-10-13 11:14:41 +0200

    add Heading component

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
+import Heading from "@/components/Heading";
 import PlaceCard from "@/components/PlaceCard";
 import places from "@/data/places.json";
 
 export default function Home() {
   return (
     <>
+      <Heading title="Available places" />
       {places.length > 0 ? (
         places.map((place) => <PlaceCard key={place.$id} place={place} />)
       ) : (
