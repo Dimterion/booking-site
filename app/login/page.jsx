9f459e93d0c66399f9a5ad99b2e5f2bca9543ab4
Dmitrii <dimterion59@gmail.com>
2024-10-14 12:47:26 +0200

    add login & register pages forms

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,62 @@
+import Link from "next/link";
+
 const LoginPage = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <div className="mt-20 w-full max-w-sm rounded-lg bg-white p-6 shadow-lg">
+        <form>
+          <h2 className="mb-6 text-center text-2xl font-bold text-gray-800">
+            Login
+          </h2>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="mb-2 block font-bold text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full rounded border px-3 py-2"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="mb-2 block font-bold text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full rounded border px-3 py-2"
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-5">
+            <button
+              type="submit"
+              className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+            >
+              Login
+            </button>
+            <p>
+              Don&apos;t have an account?{" "}
+              <Link href="/register" className="text-blue-500">
+                Register
+              </Link>
+              .
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
