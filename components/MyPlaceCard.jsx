a96c5cc5e9a5b1ba9dd749f3dd3e505d9935fa8b
Dmitrii <dimterion59@gmail.com>
2024-10-21 11:26:31 +0200

    adjust PlaceCard & MyPlaceCard style

--- components/MyPlaceCard.jsx
+++ components/MyPlaceCard.jsx
@@ -4,20 +4,19 @@ import DeletePlaceBtn from "./DeletePlaceBtn";
 
 const MyPlaceCard = ({ place }) => {
   return (
-    <div className="mt-4 flex flex-col items-center justify-between rounded-lg bg-white p-4 shadow sm:flex-row">
-      <div className="flex flex-col">
-        <h4 className="text-lg font-semibold">{place.name}</h4>
-      </div>
-      <div className="mt-2 flex w-full flex-col sm:mt-0 sm:w-auto sm:flex-row sm:space-x-2">
+    <section className="mt-4 flex flex-col items-center justify-between rounded-lg bg-white p-4 shadow sm:flex-row">
+      <h2 className="text-lg font-semibold">{place.name}</h2>
+      <section className="mt-2 flex w-full flex-col sm:mt-0 sm:w-auto sm:flex-row sm:space-x-2">
         <Link
           href={`/places/${place.$id}`}
-          className="mb-2 w-full rounded bg-blue-500 px-4 py-2 text-center text-white hover:bg-blue-700 sm:mb-0 sm:w-auto"
+          className="mb-2 flex w-full min-w-28 items-center justify-center gap-1 rounded bg-blue-800 px-4 py-2 text-center text-white hover:bg-blue-950 sm:mb-0 sm:w-auto"
         >
-          <FaEye className="mr-1 inline" /> View
+          <FaEye className="mr-1 inline" />
+          View
         </Link>
         <DeletePlaceBtn placeId={place.$id} />
-      </div>
-    </div>
+      </section>
+    </section>
   );
 };
 
