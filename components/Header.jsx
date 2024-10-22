b2b65cbd4e2dc3204dff87d25dc49511e42a136b
Dmitrii <dimterion59@gmail.com>
2024-10-22 12:25:03 +0200

    adjust header elements

--- components/Header.jsx
+++ components/Header.jsx
@@ -34,15 +34,15 @@ const Header = () => {
               <Image
                 src={logo}
                 className="h-12 w-12"
-                alt="Booking Site logo"
+                alt="Booking site logo"
                 priority={true}
               />
             </Link>
-            <div className="hidden md:block">
+            <div className="hidden sm:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <Link
                   href="/"
-                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 transition-colors hover:bg-gray-700 hover:text-white"
+                  className="rounded-md p-2 font-medium text-gray-800 transition-colors hover:bg-gray-700 hover:text-white"
                 >
                   Places
                 </Link>
@@ -50,13 +50,13 @@ const Header = () => {
                   <>
                     <Link
                       href="/bookings"
-                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 transition-colors hover:bg-gray-700 hover:text-white"
+                      className="rounded-md p-2 font-medium text-gray-800 transition-colors hover:bg-gray-700 hover:text-white"
                     >
                       Bookings
                     </Link>
                     <Link
                       href="/places/add"
-                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 transition-colors hover:bg-gray-700 hover:text-white"
+                      className="rounded-md p-2 font-medium text-gray-800 transition-colors hover:bg-gray-700 hover:text-white"
                     >
                       Add Place
                     </Link>
@@ -71,13 +71,13 @@ const Header = () => {
                 <>
                   <Link
                     href="/login"
-                    className="mr-3 text-gray-800 transition-colors hover:text-gray-600"
+                    className="flex items-center text-sm text-gray-800 transition-colors hover:text-gray-600 sm:text-base"
                   >
                     <FaSignInAlt className="mr-1 inline" /> Login
                   </Link>
                   <Link
                     href="/register"
-                    className="mr-3 text-gray-800 transition-colors hover:text-gray-600"
+                    className="mx-3 flex items-center text-sm text-gray-800 transition-colors hover:text-gray-600 sm:text-base"
                   >
                     <FaUser className="mr-1 inline" /> Register
                   </Link>
@@ -86,7 +86,7 @@ const Header = () => {
               {isAuthenticated && (
                 <>
                   <Link
-                    className="flex items-center text-xs text-gray-800 transition-colors hover:text-gray-600 sm:text-base"
+                    className="flex items-center text-sm text-gray-800 transition-colors hover:text-gray-600 sm:text-base"
                     href="/places/my"
                   >
                     <FaCompass className="mr-1 inline" />
@@ -94,7 +94,7 @@ const Header = () => {
                   </Link>
                   <button
                     onClick={handleLogout}
-                    className="mx-3 flex items-center text-xs text-gray-800 transition-colors hover:text-gray-600 sm:text-base"
+                    className="mx-3 flex items-center text-sm text-gray-800 transition-colors hover:text-gray-600 sm:text-base"
                   >
                     <FaSignOutAlt className="mr-1 inline" />
                     Sign out
@@ -105,11 +105,11 @@ const Header = () => {
           </div>
         </div>
       </nav>
-      <div className="md:hidden">
-        <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+      <div className="sm:hidden">
+        <div className="flex flex-wrap items-center justify-center px-2 pb-3 pt-2 sm:px-3">
           <Link
             href="/"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 transition-colors hover:bg-gray-700 hover:text-white"
+            className="block rounded-md px-3 py-2 text-sm font-medium text-gray-800 transition-colors hover:bg-gray-700 hover:text-white sm:text-base"
           >
             Places
           </Link>
@@ -117,13 +117,13 @@ const Header = () => {
             <>
               <Link
                 href="/bookings"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 transition-colors hover:bg-gray-700 hover:text-white"
+                className="block rounded-md px-3 py-2 text-sm font-medium text-gray-800 transition-colors hover:bg-gray-700 hover:text-white sm:text-base"
               >
                 Bookings
               </Link>
               <Link
                 href="/places/add"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 transition-colors hover:bg-gray-700 hover:text-white"
+                className="block rounded-md px-3 py-2 text-sm font-medium text-gray-800 transition-colors hover:bg-gray-700 hover:text-white sm:text-base"
               >
                 Add Place
               </Link>
