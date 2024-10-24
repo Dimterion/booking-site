74b1f2d43cbe7fc7508088d619acd0580c6aebc4
Dmitrii <dimterion59@gmail.com>
2024-10-24 14:23:21 +0200

    adjust not found page elements

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,33 +3,23 @@ import { FaArrowLeft } from "react-icons/fa";
 
 export default function NotFound() {
   return (
-    <section className="pb-24 pt-40">
-      <div className="min-h-full px-4 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
-        <div className="mx-auto max-w-max">
-          <main className="sm:flex">
-            <p className="text-4xl font-bold tracking-tight sm:text-5xl">404</p>
-            <div className="sm:ml-6">
-              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
-                <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
-                  Page not found.
-                </h1>
-                <p className="mt-1 text-base">
-                  Please check the link and try again.
-                </p>
-              </div>
-              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-                <Link
-                  href="/"
-                  className="inline-flex items-center gap-3 font-medium transition-colors"
-                >
-                  <FaArrowLeft />
-                  <span>Home page</span>
-                </Link>
-              </div>
-            </div>
-          </main>
-        </div>
-      </div>
+    <section className="mx-auto max-w-max px-8 py-12 sm:flex sm:py-24">
+      <p className="text-4xl font-bold sm:text-5xl">404</p>
+      <section className="sm:ml-6">
+        <article className="sm:border-l sm:border-gray-200 sm:pl-6">
+          <h1 className="text-4xl font-bold sm:text-5xl">Page not found.</h1>
+          <p className="mt-4 text-base">Please check the link and try again.</p>
+        </article>
+        <article className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+          <Link
+            href="/"
+            className="inline-flex items-center gap-3 font-medium text-gray-800 transition-colors hover:text-gray-600"
+          >
+            <FaArrowLeft />
+            Home page
+          </Link>
+        </article>
+      </section>
     </section>
   );
 }
