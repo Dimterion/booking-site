76a03e8ad3e4c59bf8419a9691d44fe8053c5e51
Dmitrii <dimterion59@gmail.com>
2024-11-30 11:05:43 +0100

    adjust About comp

--- components/About.jsx
+++ components/About.jsx
@@ -13,7 +13,7 @@ const stats = [
 
 export default function About() {
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+    <section className="relative isolate overflow-hidden bg-gray-700 py-24 sm:py-32">
       <div
         aria-hidden="true"
         className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
@@ -23,7 +23,7 @@ export default function About() {
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
-          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-blue-800 to-[#776fff] opacity-20"
         />
       </div>
       <div
@@ -35,7 +35,7 @@ export default function About() {
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
-          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-white to-[#776fff] opacity-20"
         />
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -67,6 +67,6 @@ export default function About() {
           </dl>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
