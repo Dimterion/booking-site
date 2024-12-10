d1edf31640e6e21f8633e93eee57e2675863565c
Dmitrii <dimterion59@gmail.com>
2024-12-10 11:56:21 +0100

    adjust Logos comp elements

--- components/Logos.jsx
+++ components/Logos.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import logo from "../assets/images/logo.png";
 
 export default function Logos() {
   return (
@@ -8,36 +9,36 @@ export default function Logos() {
       </h2>
       <section className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
         <Image
-          alt=""
-          src=""
+          alt="Logo"
+          src={logo}
           width={158}
           height={48}
           className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
         />
         <Image
-          alt=""
-          src=""
+          alt="Logo"
+          src={logo}
           width={158}
           height={48}
           className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
         />
         <Image
-          alt=""
-          src=""
+          alt="Logo"
+          src={logo}
           width={158}
           height={48}
           className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
         />
         <Image
-          alt=""
-          src=""
+          alt="Logo"
+          src={logo}
           width={158}
           height={48}
           className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
         />
         <Image
-          alt=""
-          src=""
+          alt="Logo"
+          src={logo}
           width={158}
           height={48}
           className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
