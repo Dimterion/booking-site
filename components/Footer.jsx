ffa32143a7449178b9898cc5713cb92fb809384a
Dmitrii <dimterion59@gmail.com>
2024-12-13 12:08:16 +0100

    adjust footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,34 +6,25 @@ const Footer = () => {
   return (
     <footer className="mx-auto flex w-full flex-wrap justify-around gap-3 bg-gray-100 p-6 text-sm text-gray-800 md:justify-between md:px-10">
       <section className="flex flex-wrap justify-center">
-        <p className="">&copy; {currentYear} Booking Site. Made by</p>
+        <p>&copy; {currentYear} Booking Site. Made by</p>
         <a
           href="https://github.com/Dimterion"
           target="_blank"
           rel="noopener noreferrer"
-          className="ml-1 underline transition-colors hover:text-gray-800"
+          className="ml-1 underline transition-colors hover:text-gray-600"
         >
           Dimterion
         </a>
         .
       </section>
       <section className="flex flex-wrap gap-3">
-        <Link
-          className="text-gray-800 transition-colors hover:text-gray-600"
-          href="/places"
-        >
+        <Link className="transition-colors hover:text-gray-600" href="/places">
           Places
         </Link>
-        <Link
-          className="text-gray-800 transition-colors hover:text-gray-600"
-          href="/about"
-        >
+        <Link className="transition-colors hover:text-gray-600" href="/about">
           About
         </Link>
-        <Link
-          className="text-gray-800 transition-colors hover:text-gray-600"
-          href="/contact"
-        >
+        <Link className="transition-colors hover:text-gray-600" href="/contact">
           Contact
         </Link>
       </section>
