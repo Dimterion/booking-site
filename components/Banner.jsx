209475253928208684bf3d9242c1ce1e24b279e5
Dmitrii <dimterion59@gmail.com>
2024-12-14 10:10:31 +0100

    adjust banner comp

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,7 +2,7 @@ import { FaXmark } from "react-icons/fa6";
 
 export default function Banner() {
   return (
-    <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+    <section className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
       <div
         aria-hidden="true"
         className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
@@ -12,7 +12,7 @@ export default function Banner() {
             clipPath:
               "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
           }}
-          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-blue-800 to-[#9089fc] opacity-30"
         />
       </div>
       <div
@@ -24,7 +24,7 @@ export default function Banner() {
             clipPath:
               "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
           }}
-          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-blue-800 to-[#9089fc] opacity-30"
         />
       </div>
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
@@ -41,7 +41,7 @@ export default function Banner() {
         </p>
         <a
           href="#"
-          className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+          className="flex-none rounded-full bg-gray-800 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
         >
           Lorem <span aria-hidden="true">&rarr;</span>
         </a>
@@ -52,9 +52,12 @@ export default function Banner() {
           className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
         >
           <span className="sr-only">Dismiss</span>
-          <FaXmark aria-hidden="true" className="size-5 text-gray-900" />
+          <FaXmark
+            aria-hidden="true"
+            className="size-5 text-gray-800 hover:text-gray-600"
+          />
         </button>
       </div>
-    </div>
+    </section>
   );
 }
